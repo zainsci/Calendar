f8437a88572609208b0411e015f374d4073821ce
zainsci <60652827+zainsci@users.noreply.github.com>
2020-08-31 16:57:38 +0500

    refactor calendar code to functions

---
+++ src/js/calendar.js
@@ -0,0 +1,122 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const calendar = getCalender(2020, 8);
+  printCalendar(calendar);
+});
+
+// Month In DOM
+function printCalendar(calendar) {
+  const monthGridDOM = document.getElementById("monthGrid");
+  monthGridDOM.innerHTML = "";
+
+  const div = document.createElement("div");
+  div.className = "days-row";
+  const ul = document.createElement("ul");
+
+  for (let i = 0; i < calendar.length; i++) {
+    const li = document.createElement("li");
+
+    if (calendar[i] > 7 && i < 7) {
+      li.classList = "previous-month date box";
+    } else if (calendar[i] < 7 && i > 28) {
+      li.classList = "next-month date box";
+    } else {
+      li.classList = "date box";
+    }
+
+    li.innerHTML = calendar[i];
+    li.addEventListener("click", () => {
+      showDateAndEvents(calendar[i]);
+    });
+    ul.appendChild(li);
+  }
+
+  div.appendChild(ul);
+  monthGridDOM.appendChild(div);
+}
+
+function showDateAndEvents(day) {
+  document.getElementById("selectedDay").innerHTML = day;
+}
+
+//
+function getCalender(y, m) {
+  const calendarGrid = [];
+  y = parseInt(y, 10);
+  m = parseInt(m, 10);
+
+  const preYear = y - 1;
+  const nextYear = y + 1;
+  const preMonth = m == 1 ? 12 : m - 1;
+  const nextMonth = m == 12 ? 1 : m + 1;
+
+  const wDay = getWeekDay(y, m, 1);
+
+  const DaysInMonth = getMonthDays(y, m);
+  const DaysInPrevMonth = getMonthDays(preYear, preMonth);
+
+  const preMonthDaysInGrid = wDay - 1;
+
+  for (
+    let i = DaysInPrevMonth - preMonthDaysInGrid;
+    i <= DaysInPrevMonth;
+    i++
+  ) {
+    calendarGrid.push(i);
+  }
+  for (let i = 1; i <= DaysInMonth; i++) {
+    calendarGrid.push(i);
+  }
+  const l = calendarGrid.length;
+  for (let i = l; i < 42; i++) {
+    calendarGrid.push(i - l + 1);
+  }
+
+  return calendarGrid;
+}
+
+// To get the day of the week
+function getWeekDay(y, m, d) {
+  let day = new Date(y, m - 1, d).getDay();
+  if (day == 0) {
+    return 7;
+  } else {
+    return day;
+  }
+}
+
+// To get the days in a month
+function getMonthDays(y, m) {
+  const daysIn = [
+    31,
+    isLeap(y) ? 29 : 28,
+    31,
+    30,
+    31,
+    30,
+    31,
+    31,
+    30,
+    31,
+    30,
+    31,
+  ];
+
+  return daysIn[m - 1];
+}
+
+// To check if the year is a leap year
+function isLeap(y) {
+  return (y % 4 == 0 && y % 100 != 0) || y % 400 == 0;
+}
+
+function findPreMonthDays() {
+  let sunday = date.getDate() - date.getDay();
+  sunday = ((sunday % 7) - 7 - 1) % -7;
+  sunday = `${sunday}`;
+  let noOfDays = +sunday.slice(1);
+
+  if (noOfDays == 0) {
+    noOfDays = 7;
+  }
+  return noOfDays;
+}
