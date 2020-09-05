25a3b07c7ba2b87e17dd540e1958098a8897c072
zainsci <60652827+zainsci@users.noreply.github.com>
2020-09-05 17:50:49 +0500

    add minimize and close button

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ const electron = require("electron");
 
 function createWindow() {
   const win = new electron.BrowserWindow({
-    width: 896,
-    height: 674,
+    frame: false,
+    width: 880,
+    height: 645,
     webPreferences: {
       nodeIntegration: true,
     },
