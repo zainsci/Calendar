948ce113eeccbce2b87e55dff2920654631d242e
zainsci <60652827+zainsci@users.noreply.github.com>
2022-03-04 22:59:28 +0500

    feat: Add globalStore to header

--- src/components/HeaderList.svelte
+++ src/components/HeaderList.svelte
@@ -1,18 +1,26 @@
 <script>
-  import globalStore from "../lib/store";
+  import globalStore from "../lib/store"
 
-  export let name = "";
-  export let list = [];
-  export let type = "";
-  export let temp = null;
+  export let name = ""
+  export let list = []
+  export let type = ""
+  let isOpen = false
 
-  function updateDate(type) {
-    globalStore.update((data) => {
-      data[type] = temp;
-    });
+  function updateDate(x) {
+    name = x
+    isOpen = false
+
+    if (typeof x === "string") {
+      x = new Date(`${x} 1 2022`).getMonth()
+    }
+
+    const newGlobalState = {
+      ...$globalStore,
+      [type]: x,
+    }
+
+    globalStore.set(newGlobalState)
   }
-
-  let isOpen = false;
 </script>
 
 <div class="w-full h-10 bg-gray-200 dark:bg-gray-800 relative">
@@ -29,6 +37,7 @@
       {#each list as x}
         <li
           class="w-full h-full bg-gray-200 dark:bg-gray-800 px-4 py-2 cursor-pointer hover:bg-gray-300 dark:hover:bg-gray-600 border-b border-b-gray-400 dark:border-b-gray-600"
+          on:click={() => updateDate(x)}
         >
           {x}
         </li>
--- src/components/main/Calendar.svelte
+++ src/components/main/Calendar.svelte
@@ -1,48 +1,48 @@
 <script>
-  import dayjs from "dayjs";
-  import globalStore from "../../lib/store";
+  import dayjs from "dayjs"
+  import globalStore from "../../lib/store"
 
-  let data = {};
+  let data = {}
 
-  globalStore.subscribe((d) => (data = { ...d }));
+  globalStore.subscribe((d) => (data = { ...d }))
 
   function getCalender(y, m) {
-    const grid = [];
-    y = parseInt(y, 10);
-    m = parseInt(m, 10);
+    const grid = []
+    y = parseInt(y, 10)
+    m = parseInt(m, 10)
 
-    const preYear = m === 0 ? y - 1 : y;
-    const preMonth = m === 0 ? 11 : m - 1;
+    const preYear = m === 0 ? y - 1 : y
+    const preMonth = m === 0 ? 11 : m - 1
 
-    const wDay = getWeekDay(y, m, 0);
+    const wDay = getWeekDay(y, m, 0)
 
-    const DaysInMonth = dayjs(`${y}-${m + 1}`).daysInMonth();
-    const DaysInPrevMonth = dayjs(`${preYear}-${preMonth + 1}`).daysInMonth();
+    const DaysInMonth = dayjs(`${y}-${m + 1}`).daysInMonth()
+    const DaysInPrevMonth = dayjs(`${preYear}-${preMonth + 1}`).daysInMonth()
 
-    const preMonthDaysInGrid = wDay - 1;
+    const preMonthDaysInGrid = wDay - 1
 
     for (
       let i = DaysInPrevMonth - preMonthDaysInGrid;
       i <= DaysInPrevMonth;
       i++
     ) {
-      grid.push(i);
+      grid.push(i)
     }
     for (let i = 1; i <= DaysInMonth; i++) {
-      grid.push(i);
+      grid.push(i)
     }
-    const l = grid.length;
+    const l = grid.length
     for (let i = l; i < 42; i++) {
-      grid.push(i - l + 1);
+      grid.push(i - l + 1)
     }
 
-    return grid;
+    return grid
   }
 
   // To get the day of the week
   function getWeekDay(y, m, d) {
-    const w = new Date(y, m, d).getDay();
-    return w === 0 ? 7 : w;
+    const w = new Date(y, m, d).getDay()
+    return w === 0 ? 7 : w
   }
 </script>
 
