bec76d7a79aaa260cbd290d4edab4030fc3bf386
zainsci <60652827+zainsci@users.noreply.github.com>
2022-03-05 20:56:41 +0500

    refactor: Entry into a separate component

---
+++ src/components/calendar/Entry.svelte
@@ -0,0 +1,44 @@
+<script>
+  import globalStore from "../../lib/store"
+
+  export let day
+  export let idx
+
+  function getClassNames(day, idx) {
+    let className = [
+      "px-4 py-2 bg-gray-200 dark:bg-gray-800 sel ect-none flex flex-col justify-between hover:bg-gray-300 dark:hover:bg-gray-600 cursor-pointer",
+    ]
+
+    if ((idx + 1) % 7 === 0 || (idx + 1) % 7 === 6) {
+      className.push("text-gray-500 dark:text-gray-400")
+    }
+
+    if ((idx > 25 && day < 25) || (idx < 10 && day > 10)) {
+      className.push("text-gray-500 dark:text-gray-400")
+    }
+
+    return className.join(" ")
+  }
+
+  function getCurrentDateClass(day, idx) {
+    const className = ["w-6 h-6 flex justify-center items-center rounded-full"]
+
+    let d = new Date()
+    if (
+      $globalStore.month === d.getMonth() &&
+      day === d.getDay() - 1 &&
+      $globalStore.year === d.getFullYear() &&
+      idx <= day
+    ) {
+      className.push("bg-red-500 text-white")
+    }
+
+    return className.join(" ")
+  }
+</script>
+
+<li class={getClassNames(day, idx)}>
+  <span class={getCurrentDateClass(day, idx)}>
+    {day}
+  </span>
+</li>
--- src/components/main/Calendar.svelte
+++ src/components/main/Calendar.svelte
@@ -1,10 +1,8 @@
 <script>
   import dayjs from "dayjs"
+
   import globalStore from "../../lib/store"
-
-  let selectedDate = {}
-
-  globalStore.subscribe((d) => (selectedDate = { ...d }))
+  import Entry from "../calendar/Entry.svelte"
 
   function getCalender(y, m) {
     const grid = []
@@ -44,47 +42,11 @@
     const w = new Date(y, m, d).getDay()
     return w === 0 ? 7 : w
   }
-
-  function getClassNames(day, i) {
-    let className = [
-      "px-4 py-2 bg-gray-200 dark:bg-gray-800 sel ect-none flex flex-col justify-between hover:bg-gray-300 dark:hover:bg-gray-600 cursor-pointer",
-    ]
-
-    if ((i + 1) % 7 === 0 || (i + 1) % 7 === 6) {
-      className.push("text-gray-500 dark:text-gray-400")
-    }
-
-    if ((i > 25 && day < 25) || (i < 10 && day > 10)) {
-      className.push("text-gray-500 dark:text-gray-400")
-    }
-
-    return className.join(" ")
-  }
-
-  function getCurrentDateClass(day, i) {
-    const className = ["w-6 h-6 flex justify-center items-center rounded-full"]
-
-    let d = new Date()
-    if (
-      selectedDate.month === d.getMonth() &&
-      day === d.getDay() - 1 &&
-      selectedDate.year === d.getFullYear() &&
-      i <= day
-    ) {
-      className.push("bg-red-500 text-white")
-    }
-
-    return className.join(" ")
-  }
 </script>
 
 <ul class="w-full h-full grid grid-cols-7 list-none gap-2">
-  {#each getCalender(selectedDate.year, selectedDate.month) as day, i}
-    <li class={getClassNames(day, i)}>
-      <span class={getCurrentDateClass(day, i)}>
-        {day}
-      </span>
-    </li>
+  {#each getCalender($globalStore.year, $globalStore.month) as day, i}
+    <Entry {day} idx={i} />
   {/each}
 </ul>
 
