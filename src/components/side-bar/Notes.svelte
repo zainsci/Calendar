63b1a12f710a549fc5c7cc5410b8762b700b6a91
zainsci <60652827+zainsci@users.noreply.github.com>
2022-03-10 19:30:02 +0500

    style: add dark: for notes in sidebar

--- src/components/side-bar/Notes.svelte
+++ src/components/side-bar/Notes.svelte
@@ -19,7 +19,7 @@
 <div class="flex flex-col gap-2 mt-4">
   {#each getThisMonthNotes(allNotes, $dateStore) as note}
     <div
-      class="px-4 py-2 rounded-lg bg-gray-200 hover:bg-gray-300 cursor-pointer"
+      class="px-4 py-2 rounded-lg bg-gray-200 hover:bg-gray-300 dark:bg-gray-800 dark:hover:bg-gray-700 cursor-pointer"
       on:click={() => showNote(note.id)}
     >
       {note.title}
